4b3d05462ccd4775f0c31dfbc22397e39ed2cf81
DESKTOP-DJ10EF5\krikz <denis.grishko@pra-lab.ru>
2025-07-22 23:07:33 +0300

    urdf

--- src/rob_box_bringup/setup.py
+++ src/rob_box_bringup/setup.py
@@ -1,3 +1,5 @@
+import glob
+import os
 from setuptools import setup
 
 package_name = 'rob_box_bringup'
@@ -9,13 +11,19 @@ setup(
     data_files=[
         ('share/' + package_name, ['package.xml']),
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),  # Добавляем launch-файлы
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Your Name',
     maintainer_email='your_email@example.com',
-    description='Meta-package for launching the entire rob_box system',
+    description=(
+        'Meta-package for launching the entire rob_box system. '
+        'This package is responsible for organizing and launching all components of the rob_box project.'
+    ),
     license='Apache-2.0',
     tests_require=['pytest'],
-    entry_points={},
+    entry_points={
+        # Entry points can be added here if needed
+    },
 )
