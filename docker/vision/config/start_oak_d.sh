2eac060d3263a103c12d25d03de99f8cf0aa16e4
DESKTOP-DJ10EF5\krikz <denis.grishko@pra-lab.ru>
2025-10-07 14:08:44 +0300

    10 fps

--- docker/vision/config/start_oak_d.sh
+++ docker/vision/config/start_oak_d.sh
@@ -2,5 +2,7 @@
 source /opt/ros/jazzy/setup.bash
 # Запускаем OAK-D с использованием Zenoh
 exec ros2 launch depthai_ros_driver camera.launch.py \
-  params_file:=/config/oak_d_config.yaml \
-  rs_compat:=true
+  rs_compat:=true \
+  rgb_camera.color_profile:=1280,720,10 \
+  depth_module.depth_profile:=1280,720,10 \
+  depth_module.infra_profile:=1280,720,10
