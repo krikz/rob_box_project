f8d137ded1e980faaefb4aeb5c337b4a2fd3fe93
КУКОРЕКЕН <kukoreken@rob-box.local>
2025-10-20 16:26:45 +0300

    fix(apriltag): использовать /camera/rgb/image_raw вместо /camera/camera/color/image_rect

--- docker/vision/scripts/apriltag/start_apriltag.sh
+++ docker/vision/scripts/apriltag/start_apriltag.sh
@@ -2,8 +2,8 @@
 source /opt/ros/humble/setup.bash
 
 # Запускаем AprilTag с использованием Zenoh
+# Используем /camera/rgb/image_raw для детекции маркеров
 exec ros2 run apriltag_ros apriltag_node --ros-args \
-  -r image_rect:=/camera/camera/color/image_rect \
-  -r camera_info:=/camera/camera/color/camera_info \
+  -r image_rect:=/camera/rgb/image_raw \
+  -r camera_info:=/camera/rgb/camera_info \
   --params-file /config/apriltag/apriltag_config.yaml
-  --params-file /config/apriltag_config.yaml
